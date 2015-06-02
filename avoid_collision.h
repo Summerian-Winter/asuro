64ad0dc45545d7cadbb61bfabd94237cd4c75d30
Luchs <Luchs@euirc.eu>
2015-06-02 10:59:53 +0200

    Improve collision avoidance

--- avoid_collision.h
+++ avoid_collision.h
@@ -2,27 +2,24 @@
 
 static void avoid_collision() {
 	uint8_t t1, t2;
-	t1 = PollSwitch(); t2 = PollSwitch();
+	t1 = PollSwitch(); Msleep(10); t2 = PollSwitch();
 	// Ignore K6 while driving.
 	while (t1 && t1 == t2 && t1 & ~K6 && t1 < 64) {
 		SerPrint("Collision: "); PrintInt(t1);
 		// Stop to get better values.
-		MotorSpeed(0, 0);
+		SetMotorPower(0, 0);
 		Msleep(100);
 
-		t1 = PollSwitch(); t2 = PollSwitch();
+		t1 = PollSwitch(); Msleep(10); t2 = PollSwitch();
 		SerPrint(" "); PrintInt(t1); SerPrint("\n");
 
-		uint8_t speedl = 0, speedr = 0;
-		if (t1 & K1) speedl -= 10;
-		if (t1 & K2) speedl -= 10;
-		if (t1 & K3) speedl -= 10;
-		if (t1 & K4) speedr -= 10;
-		if (t1 & K5) speedr -= 10;
-		if (t1 & K6) speedr -= 10;
-		MotorSpeed(speedl, speedr);
+		int8_t speedl = 0, speedr = 0;
+		if (t1 & (K1 | K2 | K3)) speedl = -70;
+		if (t1 & (K4 | K5 | K6)) speedr = -70;
+		SetMotorPower(speedl, speedr);
 
 		Msleep(500);
 		t1 = PollSwitch(); t2 = PollSwitch();
 	}
 }
+
