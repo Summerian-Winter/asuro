38657690314709e781fec2c1d83d1919c860914a
Luchs <Luchs@euirc.eu>
2015-06-02 10:58:45 +0200

    Move avoid_collision() into header file for code sharing

---
+++ avoid_collision.h
@@ -0,0 +1,28 @@
+// Code sharing via header file
+
+static void avoid_collision() {
+	uint8_t t1, t2;
+	t1 = PollSwitch(); t2 = PollSwitch();
+	// Ignore K6 while driving.
+	while (t1 && t1 == t2 && t1 & ~K6 && t1 < 64) {
+		SerPrint("Collision: "); PrintInt(t1);
+		// Stop to get better values.
+		MotorSpeed(0, 0);
+		Msleep(100);
+
+		t1 = PollSwitch(); t2 = PollSwitch();
+		SerPrint(" "); PrintInt(t1); SerPrint("\n");
+
+		uint8_t speedl = 0, speedr = 0;
+		if (t1 & K1) speedl -= 10;
+		if (t1 & K2) speedl -= 10;
+		if (t1 & K3) speedl -= 10;
+		if (t1 & K4) speedr -= 10;
+		if (t1 & K5) speedr -= 10;
+		if (t1 & K6) speedr -= 10;
+		MotorSpeed(speedl, speedr);
+
+		Msleep(500);
+		t1 = PollSwitch(); t2 = PollSwitch();
+	}
+}
