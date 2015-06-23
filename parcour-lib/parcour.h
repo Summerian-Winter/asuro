95030d4f31ea74fa3391389d75396afa28aa16b0
Luchs <Luchs@euirc.eu>
2015-06-23 10:22:46 +0200

    Move line following to parcour-lib

--- parcour-lib/parcour.h
+++ parcour-lib/parcour.h
@@ -1,7 +1,15 @@
 #pragma once
+#include <stdbool.h>
+
 #define BASE_SPEED 35
 
 // Wall following
 enum dir { DIR_Left, DIR_Right };
 enum follow_result { HIT_WALL, NO_WALL };
 enum follow_result follow_wall(enum dir dir);
+
+// Line following
+void search_line();
+void follow_line();
+bool check_line_end();
+
